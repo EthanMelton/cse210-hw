f7557c13e524872e03578198c7b96f984e7e94de
EthanMelton <ethanmelton7812@gmail.com>
2023-09-16 21:34:58 -0600

    added camel case

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,13 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("What is your first name? ");
-        string firstname = Console.ReadLine();
-        Console.WriteLine("");
-        
-        Console.WriteLine("What is your last name? ");
-        string lastname = Console.ReadLine();
+        string firstName = Console.ReadLine();
         Console.WriteLine("");
 
-        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
+        Console.WriteLine("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine("");
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
