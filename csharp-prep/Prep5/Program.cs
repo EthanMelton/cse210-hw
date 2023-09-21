adff36036c5c297e0937c9db71c50cdcaf7f7c90
EthanMelton <ethanmelton7812@gmail.com>
2023-09-20 21:53:30 -0600

    prep 5 finished

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string firstName = PromptFirstName();
+        string lastName = PromptLastName();
+        int userNumber = PromptUserNumber();
+        int squareNumber = SquareNumber(userNumber);
+        DisplayResults(firstName, lastName, squareNumber);
     }
-}
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptFirstName()
+    {
+        Console.WriteLine("Please enter your first name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static string PromptLastName()
+    {
+        Console.WriteLine("Please enter your last name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResults(string first, string last, int square)
+    {
+        Console.WriteLine($"Hey {first} {last}! The square of your number is {square}!");
+    }
+}   
