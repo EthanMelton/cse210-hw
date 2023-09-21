f12fbe52bdf9cfb494144774a839f19539d9460c
EthanMelton <ethanmelton7812@gmail.com>
2023-09-20 20:52:59 -0600

    added comments to prep 3 and started prep 4

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,14 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        //boolean variable
         string response = "yes";
 
+        //do while loop
         do
         {
-
+        
+        //creating random variable
         Random randomNumber = new Random();
         int winningNumber = randomNumber.Next(1, 101);
+        
+        //creating guess variable and guessAmount variable
         int guess = 0;
         int guessAmount = 0;
 
@@ -20,16 +24,19 @@ class Program
                 Console.WriteLine("What is your guess? ");
                 guess = int.Parse(Console.ReadLine());
 
+                //if statement that tells user to go lower
                 if (guess > winningNumber)
                 {
                     Console.WriteLine("Lower!");
                     guessAmount += 1;
                 }
+                //else if statement that tells user to go higher
                 else if (guess < winningNumber)
                 {
                     Console.WriteLine("Higher!");
                     guessAmount += 1;
                 }
+                //else statement that tells user that they won
                 else
                 {
                     Console.WriteLine("You Guessed it!");
@@ -37,8 +44,13 @@ class Program
                 }
             }
 
+            //statement telling user how many guesses they got the number in
             Console.WriteLine($"You got the Winning Number in {guessAmount} guesses!");
+
+            //question asking if the user wants to continue
             Console.Write("Do you want to continue? ");
+
+            //this is where the response is determined.
             response = Console.ReadLine();
 
         } while (response == "yes");
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.WriteLine("Please enter a number. (Enter 0 to quit): ");
+
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
     }
 }
