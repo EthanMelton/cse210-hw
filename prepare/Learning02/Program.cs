1797f448d80691b5da40a4f03b33314cdf3a38dd
EthanMelton <ethanmelton7812@gmail.com>
2023-09-27 22:57:23 -0600

    abstraction learning activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job FirstJob = new Job();
+        FirstJob.JobTitle = "Data Analyst";
+        FirstJob.company = "Google";
+        FirstJob.StartYear = 2003;
+        FirstJob.EndYear = 2018;
+        
+        Job SecondJob = new Job();
+        SecondJob.JobTitle = "Software Engineer";
+        SecondJob.company = "Microsoft";
+        SecondJob.StartYear = 2019;
+        SecondJob.EndYear = 2022;
+
+        Job ThirdJob = new Job();
+        ThirdJob.JobTitle = "Manager";
+        ThirdJob.company = "Apple";
+        ThirdJob.StartYear = 2022;
+        ThirdJob.EndYear = 2023;
+
+        Job FourthJob = new Job();
+        FourthJob.JobTitle = "Data Scientist";
+        FourthJob.company = "Apple";
+        FourthJob.StartYear = 2018;
+        FourthJob.EndYear = 2023;
+
+        Resume FirstResume = new Resume();
+        FirstResume.name = "Allison Rose";
+
+        Resume SecondResume = new Resume();
+        SecondResume.name = "Steven Harvey";
+
+        FirstResume.jobs.Add(SecondJob);
+        FirstResume.jobs.Add(ThirdJob);
+        SecondResume.jobs.Add(FirstJob);
+        SecondResume.jobs.Add(FourthJob);
+
+        Console.WriteLine();
+        FirstResume.Display();
+        Console.WriteLine();
+        SecondResume.Display();
+        Console.WriteLine();
     }
 }
